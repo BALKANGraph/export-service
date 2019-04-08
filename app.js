f4484f24217310971d0564e1795c6098c547e8c6
BALKANGraph <plamen.peshev@live.com>
2019-04-08 16:03:15 +0300

    added express

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 var port = process.env.PORT || 1337;
 var virtualDirPath = process.env.virtualDirPath || "";
+const express = require('express');
+const app = express();
 const puppeteerParams = {args: ['--no-sandbox', '--disable-setuid-sandbox']};
 const APP_DATA = "/appdata";
 const ERROR = "Aw Snap! Something bad has happened! See the logs!";
 const ONE_HOUR =  60 * 60 * 1000; /* ms */
 const PADDING = 5;
-
-const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const uuid = require('uuid');                              
@@ -28,49 +30,57 @@ const l = createLogger({
     ]
 });
 
-http.createServer(function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.setHeader("Access-Control-Max-Age", "3600");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    
-    clear(res);  
-    if(isOptions(req)) {
+app.use(bodyParser.json({limit: '50mb'}));
 
-        res.writeHead(200);
-        res.end();      
-    }  
-    else if(isPost(req, "/pdf")) {
-        convert(req, res, "pdf");        
-    }
-    else if(isPost(req, "/png")) {
-        convert(req, res, "png");        
-    }
-    else  {
-        servre(req, res);
-    }
-}).listen(port);
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    next();
+});
+  
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + req.url.replace(virtualDirPath, "")));
+});
+
+app.post(virtualDirPath + '/pdf', function(req, res) {
+    convert(req, res, "pdf");
+    clear();
+});
+
+app.post(virtualDirPath + '/png', function(req, res) {
+    convert(req, res, "png");
+    clear();
+});
 
 function clear(res) {  
     var dir = `${__dirname}${APP_DATA}`;
-
+    l.debug("clear");
     if (!fs.existsSync(dir)){
+        l.debug(`mkdirSync: ${dir}`);
         fs.mkdirSync(dir);
     }
-
+    
+    l.debug(`readdir: ${dir}`);
     fs.readdir(dir, (err, files) => {
         if (err) {
+            l.debug(`readdir error: ${JSON.stringify(err)}`);                   
             l.error(JSON.stringify(err));
-            res.writeHead(404);
-            res.end(ERROR);
             return;            
         }
         files.forEach(file => {
-            if (file.indexOf('+') == -1){
+            if (file.indexOf('~') == -1){
                 return;
             }
 
-            var dtstring = file.split('+')[0];
+            var dtstring = file.split('~')[0];
             var dtarray = dtstring.split("-");
             if (dtarray.length != 6){
                 return;   
@@ -80,6 +90,7 @@ function clear(res) {
             var now = new Date();
 
             var filename = `${__dirname}${APP_DATA}/${file}`;
+            l.debug(`readdir filename: ${filename}`);            
 
             if (((now - d) < ONE_HOUR)){
                 l.debug(`file ${filename} is less then one hour old and will not be deleted!`);
@@ -88,9 +99,8 @@ function clear(res) {
 
             fs.unlink(filename, (err) => {
                 if (err) {
+                    l.debug(`unlink filename: ${filename}; err: ${JSON.stringify(err)}`);
                     l.error(JSON.stringify(err));
-                    res.writeHead(404);
-                    res.end(ERROR);
                     return;            
                 }
                 l.info(`file ${filename} has been deleted!`);
@@ -99,120 +109,115 @@ function clear(res) {
     });
 
     dir = './logs';
+    l.debug(`dir existsSync: ${dir}`);
 
     if (!fs.existsSync(dir)){
+        l.debug(`mkdirSync start: ${dir}`);
         fs.mkdirSync(dir);
+        l.debug(`mkdirSync end: ${dir}`);
     }
 }
 
-function servre(req, res) {   
-    var fileName = __dirname + req.url.replace(virtualDirPath, "");
-    fs.readFile(fileName, function (err, data) {
-        if (err) {
-            l.info(`Cannot read file: ${fileName}`);
+function convert(req, res, type) {
+    var href = "http://"+ req.headers.host + virtualDirPath;
+    l.debug(`req.headers.referer: ${req.headers.referer}`);
+
+    var parsedXML = xml.parse(req.body.svg);  
+    var width = parsedXML[0].attributes.width;
+    var height = parsedXML[0].attributes.height;  
+    width = parseFloat(width) + PADDING;
+    height = parseFloat(height) + PADDING;
+    var filename = uuid.v4(); 
+    var d = new Date();
+
+    d = d.getFullYear() 
+        + "-" + d.getMonth()
+        + "-" + d.getDate()
+        + "-" + d.getHours()
+        + "-" + d.getMinutes()
+        + "-" + d.getSeconds()
+    
+    var filenameSvg = `${__dirname}${APP_DATA}/${d}~${filename}.svg`; 
+    var filenameSvgUrl = href + `${APP_DATA}/${d}~${filename}.svg`;
+    var filenameConverted = `${__dirname}${APP_DATA}/${d}~${filename}.${type}`; 
+    l.debug(`convert writeFile filenameSvg: ${filenameSvg}`);
+
+    fs.writeFile(filenameSvg, req.body.svg, function(err) {
+        if(err) {
+            l.debug(`convert writeFile err filenameSvg: ${filenameSvg}`);
+
+            l.error(JSON.stringify(err));
             res.writeHead(404);
             res.end(ERROR);
-            return;            
+            return;
         }
-        if (req.url.indexOf(".svg") != -1) {
-            res.writeHead(200, {'content-type': 'image/svg+xml'});
-        }
-        else{
-            res.writeHead(200);
-        }
-        res.end(data);
-        return;  
-    }); 
-}
+        l.debug(`convert success filenameSvg: ${filenameSvg}`);
+        l.info(`${filenameSvg} file was saved; width: ${width}; height: ${height};`);
+        (async () => {
+            const browser = await puppeteer.launch(puppeteerParams);
+            const page = await browser.newPage();
+            l.debug(`goto start filenameSvgUrl: ${filenameSvgUrl}`);
+            await page.goto(filenameSvgUrl, { waitUntil: 'networkidle2' });
+            l.debug(`goto end filenameSvgUrl: ${filenameSvgUrl}`);
 
-function convert(req, res, type) {
-    var body = "";
-    var href = "http://"+ req.headers.host + virtualDirPath;
-    req.on('data', function(chunk) {
-        body += chunk;
-    });
-    req.on('end', function() {
-        l.debug(body);
-        var data = JSON.parse(body); 
-        var parsedXML = xml.parse(data.svg);  
-        var width = parsedXML[0].attributes.width;
-        var height = parsedXML[0].attributes.height;  
-        width = parseFloat(width) + PADDING;
-        height = parseFloat(height) + PADDING;
-        var filename = uuid.v4(); 
-        var d = new Date();
+            if (type == "png"){
+                l.debug(`goto png page.screenshot start filenameConverted: ${filenameConverted}`);
 
-        d = d.getFullYear() 
-            + "-" + d.getMonth()
-            + "-" + d.getDate()
-            + "-" + d.getHours()
-            + "-" + d.getMinutes()
-            + "-" + d.getSeconds()
-        
-        var filenameSvg = `${__dirname}${APP_DATA}/${d}~${filename}.svg`; 
-        var filenameSvgUrl = href + `${APP_DATA}/${d}~${filename}.svg`;
-        var filenameConverted = `${__dirname}${APP_DATA}/${d}~${filename}.${type}`; 
-
-        fs.writeFile(filenameSvg, data.svg, function(err) {
-            if(err) {
-                l.error(JSON.stringify(err));
-                res.writeHead(404);
-                res.end(ERROR);
-                return;
-            }
-            l.info(`${filenameSvg} file was saved; width: ${width}; height: ${height};`);
-            (async () => {
-                const browser = await puppeteer.launch(puppeteerParams);
-                const page = await browser.newPage();
-                await page.goto(filenameSvgUrl, { waitUntil: 'networkidle2' });
-                if (type == "png"){
-                    await page.screenshot({
-                        path: filenameConverted,
-                        clip : {
-                            x      : 0,
-                            y      : 0,
-                            width  : width,
-                            height : height
-                        }
-                    });
-                }
-                else if (type == "pdf"){
-                    await page.pdf({
-                        path: filenameConverted,
-                        width: width,
-                        height: height,
-                        printBackground: true
-                    });
-                }
-
-                await browser.close();
-
-                fs.readFile(filenameConverted, function (err, filedata) {
-                    if (err) {
-                        l.error(JSON.stringify(err));
-                        res.writeHead(404);
-                        res.end(ERROR);
-                        return;                        
+                await page.screenshot({
+                    path: filenameConverted,
+                    clip : {
+                        x      : 0,
+                        y      : 0,
+                        width  : width,
+                        height : height
                     }
-                    l.info(`${filenameConverted} file was converted;`);
-                    res.writeHead(200);
-                    res.end(filedata);
                 });
-            })();              
-        });         
-    });
+                l.debug(`goto png page.screenshot end filenameConverted: ${filenameConverted}`);
+
+            }
+            else if (type == "pdf"){
+                l.debug(`goto pdf page.screenshot start filenameConverted: ${filenameConverted}`);
+
+                await page.pdf({
+                    path: filenameConverted,
+                    width: width,
+                    height: height,
+                    printBackground: true
+                });
+                l.debug(`goto pdf page.screenshot end filenameConverted: ${filenameConverted}`);
+
+            }
+            l.debug(`browser.close(); start filenameConverted: ${filenameConverted}`);
+
+            await browser.close();
+            l.debug(`browser.close(); end filenameConverted: ${filenameConverted}`);
+
+            l.debug(`convert readFile filenameConverted: ${filenameConverted}`);
+
+            fs.readFile(filenameConverted, function (err, filedata) {
+                if (err) {
+                    l.debug(`convert readFile err filenameConverted: ${filenameConverted}; err ${JSON.stringify(err)}`);
+                    l.error(JSON.stringify(err));
+                    res.writeHead(404);
+                    res.end(ERROR);
+                    return;                        
+                }
+                l.debug(`convert success readFile filenameConverted: ${filenameConverted}`);
+
+                l.info(`${filenameConverted} file was converted;`);
+                res.writeHead(200);
+
+                res.end(filedata);
+                l.debug(`convert success readFile filenameConverted: ${filenameConverted} end`);
+
+            });
+        })();              
+    });         
 }
 
-function isPost(req, path){
-    return (req.url == virtualDirPath + path && req.method.toLowerCase() == "post"); 
-}
 
-function isOptions(req, path){
-    return (req.method.toLowerCase() == "options"); 
-}
-
-
-process.on('uncaughtException', function(err) {    
+process.on('uncaughtException', function(err) {   
+    l.debug(`uncaughtException`);
     l.error(err.message);
     l.error(err.stack);
 });
